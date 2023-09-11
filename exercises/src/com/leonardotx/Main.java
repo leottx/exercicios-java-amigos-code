02c4e945d065d7042df76eac6692a4e468144ad8
Leonardo Teixeira <tteixeira.leonardo@gmail.com>
2023-09-11 15:48:59 -0300

    feat: 01 exercise - reverse string method

--- exercises/src/com/leonardotx/Main.java
+++ exercises/src/com/leonardotx/Main.java
@@ -4,6 +4,33 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello Java!");
+        System.out.println(reverseString("Leonardo Teixeira"));
+        System.out.println(reverseStringAlternative("Leonardo Teixeira"));
+    }
+
+    public static String reverseString(String text) {
+        String[] textArray = text.split("");
+        String reversedString = "";
+
+        for (int i = text.length() - 1; i >= 0 ; i--) {
+            reversedString += textArray[i];
+        }
+
+        return reversedString;
+    }
+
+    // Solução alternativa e mais otimizada
+    public static String reverseStringAlternative(String input) {
+        if (input == null || input.isEmpty()) {
+            return input; // Return the input as is for empty or null strings
+        }
+
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed.append(input.charAt(i));
+        }
+
+        return reversed.toString();
     }
 }
