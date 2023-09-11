85915bce812ca462676a9ab70fe6423adc148b63
Leonardo Teixeira <tteixeira.leonardo@gmail.com>
2023-09-11 11:47:34 -0300

    feat: 06 exercise - array copy

--- module-getting-started/src/com/leonardotx/Main.java
+++ module-getting-started/src/com/leonardotx/Main.java
@@ -4,8 +4,19 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String[] stringArray = {"a", "b", "c", "d"};
+        // using your answer from Q5 above, create a second array which is initialized using your original array
+        // hint: instead of using the `new` keyword, try simply giving the name of your original array
+        // change the value at index 0 of the new array to "z"
+        // print both of your arrays and compare
 
+        String[] stringArray = {"a", "b", "c", "d"};
+        String [] copyStringArray = stringArray;
+
+        copyStringArray[0] = "z";
+
+        /* Ambos são alterados pois arrays são reference types, ou seja, cada variável na stack é um ponteiro para o
+        mesmo espaço de memória no heap */
         System.out.println(Arrays.toString(stringArray));
+        System.out.println(Arrays.toString(copyStringArray));
     }
 }
