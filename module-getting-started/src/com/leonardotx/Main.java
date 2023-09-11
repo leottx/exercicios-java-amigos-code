51011ea52661673e0de81199ee9ddb54d0fdbc2a
Leonardo Teixeira <tteixeira.leonardo@gmail.com>
2023-09-11 13:48:29 -0300

    feat: 12 exercise - Uppercases each value of the String array

--- module-getting-started/src/com/leonardotx/Main.java
+++ module-getting-started/src/com/leonardotx/Main.java
@@ -4,24 +4,17 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        // using your answer to Q6, create another array which is A COPY off of your original array (using the `Arrays.copyOf()` method)
-        // NOTE that the `.copyOf()` method accepts two arguments, first being the array that's being copied and second being the length of the new array
-        // change the value at index 0 of the new array to "z"
-        // print both of the arrays and compare
+        // create a for loop which goes through each string in our array
+        // make each word uppercase
+        // assign the value in the array to the uppercase string
+        // print our resulting array
 
-        String[] stringArray = {"a", "b", "c", "d"};
-        String [] copyStringArray = Arrays.copyOf(stringArray, stringArray.length);
+        String[] stringArray = {"i", "sure", "do", "love", "bees"};
 
-        copyStringArray[0] = "z";
-
-        /* O método Arrays.copyOf() cria um novo espaço de memória do heap para que o novo array seja de fato uma cópia
-        do array e não uma cópia do ponteiro */
-        System.out.println(Arrays.toString(stringArray));
-        System.out.println(Arrays.toString(copyStringArray));
-
-        for (String str:
-             stringArray) {
-            System.out.println(str);
+        for (int i = 0; i < stringArray.length; i++) {
+            stringArray[i] = stringArray[i].toUpperCase();
         }
+
+        System.out.print(Arrays.toString(stringArray));
     }
 }
