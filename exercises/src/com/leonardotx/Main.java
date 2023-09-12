ce6a4912f990d2d3b89e9789bd9e89fa05599090
Leonardo Teixeira <tteixeira.leonardo@gmail.com>
2023-09-12 11:27:46 -0300

    feat: 05 exercise - creates a method that capitalizes the first letter of a String

--- exercises/src/com/leonardotx/Main.java
+++ exercises/src/com/leonardotx/Main.java
@@ -4,20 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter your monthly income: ");
-        double monthlyIncome = sc.nextDouble();
-        System.out.print("Enter the property value: ");
-        double propertyValue = sc.nextDouble();
-
-        System.out.println("Your anual rental yield is: " + calcAnualRentalYield(monthlyIncome, propertyValue));
+        System.out.println(capitalizeFirstLetter("hello world"));
     }
 
-    public static String calcAnualRentalYield(double monthlyIncome, double propertyValue) {
-        double anualRentalIncome = monthlyIncome * 12;
-        double anualRentalYield = (anualRentalIncome / propertyValue) * 100;
+    public static String capitalizeFirstLetter(String word) {
+        if (word == null || word.isEmpty()) {
+            return word;
+        }
 
-        return String.format("%.2f%%", anualRentalYield);
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
     }
 }
