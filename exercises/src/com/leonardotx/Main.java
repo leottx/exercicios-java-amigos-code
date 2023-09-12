7f33e203a04ca175d8b098343a8a9ad4f5d94091
Leonardo Teixeira <tteixeira.leonardo@gmail.com>
2023-09-12 10:39:39 -0300

    feat: class exercise - creates a method that calculates the anual rental yield of a property

--- exercises/src/com/leonardotx/Main.java
+++ exercises/src/com/leonardotx/Main.java
@@ -1,9 +1,23 @@
 package com.leonardotx;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello Java!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your monthly income: ");
+        double monthlyIncome = sc.nextDouble();
+        System.out.print("Enter the property value: ");
+        double propertyValue = sc.nextDouble();
+
+        System.out.println("Your anual rental yield is: " + calcAnualRentalYield(monthlyIncome, propertyValue));
+    }
+
+    public static String calcAnualRentalYield(double monthlyIncome, double propertyValue) {
+        double anualRentalIncome = monthlyIncome * 12;
+        double anualRentalYield = (anualRentalIncome / propertyValue) * 100;
+
+        return String.format("%.2f%%", anualRentalYield);
     }
 }
