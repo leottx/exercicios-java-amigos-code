e46827f3238f88c61b6924cc1e31beec7718e86c
Leonardo Teixeira <tteixeira.leonardo@gmail.com>
2023-09-13 14:38:38 -0300

    feat: 04 exercise - creates a method to calc age

--- exercises/src/com/leonardotx/Main.java
+++ exercises/src/com/leonardotx/Main.java
@@ -1,11 +1,20 @@
 package com.leonardotx;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.Month;
 
 public class Main {
     public static void main(String[] args) {
         LocalDate myBirthDate = LocalDate.of(1996, Month.APRIL, 6);
-        System.out.println("The date of my birth date plus a thousand days is: " + myBirthDate.plusDays(1000));
+        System.out.println("My age is: " + calcAge(myBirthDate));
+    }
+
+    private static int calcAge(LocalDate birthDate) {
+        if (birthDate == null) {
+            return -1;
+        }
+
+        return LocalDate.now().getYear() - birthDate.getYear();
     }
 }
