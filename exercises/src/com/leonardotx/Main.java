82fbcddab7b1742099fffd6fb2c5a0ae95bf8a1d
Leonardo Teixeira <tteixeira.leonardo@gmail.com>
2023-09-13 14:25:21 -0300

    feat: 01 exercise - prints today's localDateTime and localDate

--- exercises/src/com/leonardotx/Main.java
+++ exercises/src/com/leonardotx/Main.java
@@ -1,9 +1,13 @@
 package com.leonardotx;
 
-import java.util.Arrays;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello Java!");
+        LocalDateTime todayLocalDateTime = LocalDateTime.now();
+        System.out.println("Today localDateTime: " + todayLocalDateTime);
+        LocalDate todayLocalDate = LocalDate.now();
+        System.out.println("Today localDate: " + todayLocalDate);
     }
 }
