5c390ad67e49e45f34de92032f8c3b8d107b88bf
Leonardo Teixeira <tteixeira.leonardo@gmail.com>
2023-09-13 14:32:19 -0300

    feat: 03 exercise - prints the date of my birth date plus a thousand days

--- exercises/src/com/leonardotx/Main.java
+++ exercises/src/com/leonardotx/Main.java
@@ -6,6 +6,6 @@ import java.time.Month;
 public class Main {
     public static void main(String[] args) {
         LocalDate myBirthDate = LocalDate.of(1996, Month.APRIL, 6);
-        System.out.println("My birth date is: " + myBirthDate);
+        System.out.println("The date of my birth date plus a thousand days is: " + myBirthDate.plusDays(1000));
     }
 }
