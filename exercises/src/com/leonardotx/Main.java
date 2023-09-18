e66691e9289496b7633daaf21df6c566698bb2a5
Leonardo Teixeira <tteixeira.leonardo@gmail.com>
2023-09-18 14:32:56 -0300

    feat: class exercise - creates two methods for creating and writing on a file

--- exercises/src/com/leonardotx/Main.java
+++ exercises/src/com/leonardotx/Main.java
@@ -1,9 +1,40 @@
 package com.leonardotx;
 
-import java.util.Arrays;
+import javax.swing.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello Java!");
+        String path = "src/test.txt";
+
+        try {
+            File newFile = createFile(path);
+            writeOnFile(newFile, true, "Hello");
+            writeOnFile(newFile, true, "World");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static File createFile(String path) throws IOException {
+        File newFile = new File(path);
+
+        if (!newFile.exists()) {
+            newFile.createNewFile();
+        }
+
+        return newFile;
+    }
+
+    public static void writeOnFile(File file, boolean append, String content) throws IOException {
+        FileWriter fileWriter = new FileWriter(file, append);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+
+        printWriter.println(content);
+        printWriter.flush();
+        printWriter.close();
     }
 }
