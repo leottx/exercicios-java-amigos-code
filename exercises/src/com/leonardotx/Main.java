260f0104ccb790ace60cc45e88383d6e0f16bc6b
Leonardo Teixeira <tteixeira.leonardo@gmail.com>
2023-09-18 13:27:27 -0300

    feat: 01 exercise - creates a method to convert String arguments to int

--- exercises/src/com/leonardotx/Main.java
+++ exercises/src/com/leonardotx/Main.java
@@ -1,9 +1,34 @@
 package com.leonardotx;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello Java!");
+        StringBuilder sb = new StringBuilder();
+        String[] stringArray = new String[args.length];
+        int sum = 0;
+
+        for (int i = 0; i < args.length; i++) {
+            try {
+                sum += Integer.parseInt(args[i]);
+            } catch(NumberFormatException e){
+                stringArray[i] = args[i];
+                System.out.println(e.getMessage());
+            }
+        }
+
+        sb.append("Total sum: " + sum);
+
+        if (!Arrays.stream(stringArray).allMatch(Objects::isNull)) {
+            for (String item: stringArray) {
+                if(item != null) {
+                    sb.append(" " + item);
+                }
+            }
+            sb.append(" are not number(s)");
+        }
+
+        System.out.println(sb.toString());
     }
 }
